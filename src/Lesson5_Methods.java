8a545c8a51f72e4c4f8b522508f26987b87ace5f
CheezyMan69 <karimaref2005@gmail.com>
2024-10-29 12:17:37 +0100

    added notes

---
+++ src/Lesson5_Methods.java
@@ -0,0 +1,46 @@
+/*Notes ----------------------
+Method
+    A method is similar to a function in other prog languages
+    Instead of writing the same piece of code over and over again
+    you could just create a method of it and call it in the main code
+
+    A method has some elements but 2 of the most important ones are:
+        - Parameters
+            These tell the method what data to use in the piece of code to keep it
+            adaptive. They are specified after the name of the method in the ().
+            You can use multiple parameters with different types, they must be sepereated
+            by a , (comma).
+
+        - Return value
+            This tells the method what value type to return when done with the piece
+            of code in it. It uses the return statement to make it clear what it will
+            give out once executed.
+
+Method Overloading
+    This is a technique that allows the use of the same method but to use different data types
+    as parameters. So for example, instead of having 2 methods with the same code but one works
+    with int and the other with double, if you overload it, you can have the same method
+    work with both double and int. This is done by specifying the return type and parameter types
+    before the piece of code. This allows us to have the same name for a method but different use
+    cases.
+
+
+    I don't want to do this anymore, but I will lol
+    i will do the code a bit later
+    give me time plz
+
+ */
+
+
+
+
+public class Lesson5_Methods {
+    public static void main(String[] args){
+
+    }
+
+    public static void add(){
+
+    }
+}
+
