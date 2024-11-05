895f2d663a13ed537d72ffa577c44086e4da653f
CheezyMan69 <karimaref2005@gmail.com>
2024-11-05 12:04:16 +0100

    added example code

--- src/Lesson6_Classes.java
+++ src/Lesson6_Classes.java
@@ -27,5 +27,23 @@ import java.util.Scanner;
 public class Lesson6_Classes {
     public static void main(String[] args){
 
+        example myObj1 = new example();     //Constructing an object
+        System.out.println(myObj1.x);       //printing the x value in the object
+        example myObj2 = new example();     //Constructing another object
+        System.out.println(myObj2.y);       //printing the y value in the object
+
+        example2 lol = new example2();      //Constructing lol using the second class
+        System.out.println("A value = "+lol.a +"\nU value = " +lol.u);  //when using .value it accesses that value
     }
+}
+
+// example of initializing a class
+class example{
+    int x = 69;
+    int y = 420;
+}
+
+class example2{     //A class can have multiple variable types
+    char a = 'b';
+    double u = 69420;
 }
