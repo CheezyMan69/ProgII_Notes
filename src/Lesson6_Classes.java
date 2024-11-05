9cb5fbee1881840c3d8e453b81a60fa904caf1d2
CheezyMan69 <karimaref2005@gmail.com>
2024-11-05 10:58:44 +0100

    Added OOP notes

---
+++ src/Lesson6_Classes.java
@@ -0,0 +1,31 @@
+/* Notes -------------------------------------
+Classes and Objects
+    Java is an OOP language, an Object-Oriented Programming language
+    this allows for a lot of simplicity in the code. This introduces many more
+    ways to use and alter the code as opposed to C. The 2 main aspects of an OOP
+    language are Classes and Objects.
+
+    What are classes?
+        Classes are a set of attributes and rules/template that certain values (Objects)
+        need to follow, this allows for less repetition, easy to understand code, and quick
+        for the machine to interpret. Classes can contain variables, methods, and more. They
+        are often referred to as Object Constructors. 1 class can have multiple objects with
+        different names.
+
+    What are Objects?
+        Objects are the code that follow this template, or inherit the attributes from the
+        class. An Object can not exist without a class to set it up/construct it.
+
+
+*/
+
+
+//Code ----------------------------------------
+
+import java.util.Scanner;
+
+public class Lesson6_Classes {
+    public static void main(String[] args){
+
+    }
+}
